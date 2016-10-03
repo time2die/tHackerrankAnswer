562d7b4b21397da7baee1f18554577ef41b371bd
Aleksandr Karpov <a.karpov@semrush.com>
2016-10-03 17:57:06 +0300

    fix style

--- src/main/java/org/time2die/hackerrank/Main.java
+++ src/main/java/org/time2die/hackerrank/Main.java
@@ -10,25 +10,25 @@ public class Main {
 
     private void work() {
         String[] data = getFakeData();
-        for(String iter : data){
+        for (String iter : data) {
             System.out.println(countDeletedNum(iter));
         }
     }
 
-    private int countDeletedNum(String string){
-        return 0 ;
+    private int countDeletedNum(String string) {
+        return 0;
     }
 
-    private String[] getFakeData(){
-        return new String[]{"AAAA", "BBBBB", "ABABABAB", "BABABA", "AAABBB" } ;
+    private String[] getFakeData() {
+        return new String[]{"AAAA", "BBBBB", "ABABABAB", "BABABA", "AAABBB"};
     }
 
     private String[] getData() {
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
-        String [] returnValue = new String[size] ;
+        String[] returnValue = new String[size];
         for (int i = 0; i < size; i++) {
-            returnValue[i] = sc.nextLine() ;
+            returnValue[i] = sc.nextLine();
         }
 
         return returnValue;
